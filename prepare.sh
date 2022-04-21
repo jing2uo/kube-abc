9349aaa08f8a188c5cc57f8028a0fe8112bba18c
jing2uo <mail@guojing.io>
2022-04-21 08:05:13 +0800

    修改 origin 文件夹名称防止 git 报错

--- prepare.sh
+++ prepare.sh
@@ -1,7 +1,7 @@
 source ./workflow/1-function.sh
-sed "s/CHANGEME/${OWNIP}:8989/g" ./workflow/origin/join.sh >./workflow/join.sh
-sed "s/CHANGEME/${OWNIP}:8989/g" ./workflow/origin/onenode.sh >./workflow/onenode.sh
-sed "s/CHANGEME/${OWNIP}:8989/g" ./workflow/origin/index.html >./workflow/index.html
-sed "s/CHANGEME/${OWNIP}:8989/g" ./workflow/origin/cmd >./workflow/cmd
+sed "s/CHANGEME/${OWNIP}:8989/g" ./workflow/origin-files/join.sh >./workflow/join.sh
+sed "s/CHANGEME/${OWNIP}:8989/g" ./workflow/origin-files/onenode.sh >./workflow/onenode.sh
+sed "s/CHANGEME/${OWNIP}:8989/g" ./workflow/origin-files/index.html >./workflow/index.html
+sed "s/CHANGEME/${OWNIP}:8989/g" ./workflow/origin-files/cmd >./workflow/cmd
 
 cd ./workflow && python2 -m SimpleHTTPServer 8989
