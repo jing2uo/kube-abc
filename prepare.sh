8d5a07d788f7f1c70decfdd1f5bdea3310a5e079
jing2uo <mail@guojing.io>
2022-04-21 08:29:11 +0800

    添加 docker run nginx

--- prepare.sh
+++ prepare.sh
@@ -5,3 +5,7 @@ sed "s/CHANGEME/${OWNIP}:8989/g" ./workflow/origin-files/index.html >./workflow/
 sed "s/CHANGEME/${OWNIP}:8989/g" ./workflow/origin-files/cmd >./workflow/cmd
 
 cd ./workflow && python2 -m SimpleHTTPServer 8989
+
+#cd ./workflow && python3 -m http.server 8989
+
+#docker run --restart=always --name kubeabc -p 8989:80 -v ${PWD}/workflow:/usr/share/nginx/html:ro -d nginx
